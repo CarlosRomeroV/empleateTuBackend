977087c2fc8373bf09f6671724eda55b6a6710f3
CarlosRomeroV <cromveloso@gmail.com>
2025-01-31 09:14:29 +0100

    v3

--- src/routes/offert.routes.ts
+++ src/routes/offert.routes.ts
@@ -5,18 +5,18 @@ const router = Router()
 
  
 //GET Listar todas las ofertas localhost:3000/api/offerts/?title=react&category=dam
-router.get('/', OfferController.getAll)
+//router.get('/', OfferController.getAll)
 //POST añadir una oferta nueva localhost:3000/api/offerts/  {body}
-router.post('/', OfferController.save)
+//router.post('/', OfferController.save)
 //DELETE Borrar una oferta localhost:3000/api/offerts/XXXX  
-router.delete('/:id', OfferController.delete)
+//router.delete('/:id', OfferController.delete)
 //PUT modificar una oferta localhost:3000/api/offerts/XXXX  {body}
-router.put('/:id', OfferController.update)   
+//router.put('/:id', OfferController.update)   
 
 // Calificamos una oferta x   {body}
-router.post('/:id/rate/',OfferController.rate)  
+//router.post('/:id/rate/',OfferController.rate)  
 // Vemos que calificación (total) se le ha data a una oferta X
-router.get('/:id/rate/', OfferController.getRate)
+//router.get('/:id/rate/', OfferController.getRate)
 
 
 export default router
